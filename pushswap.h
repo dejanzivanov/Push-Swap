9ab83f09eade9d1943e26982c88486b86f6287de
Dejan Zivanov <punisher0504@gmail.com>
2021-11-09 21:35:13 +0100

    Creating Basic Skeleton

--- pushswap.h
+++ pushswap.h
@@ -1,5 +1,19 @@
 typedef struct s_list
 {
-	int value;     
+	int value;
 	void *next;
 }			t_list;
+
+void	argument_handling(char **argv);
+void	error_handling(void); //error handler Already Sorted/Errong Handling / No Arguments
+void	sa(void);  //swap a - swap the first 2 elements at the top of stack a. Do nothing if there is only one or no elements).
+void	sb(void);  //swap b - swap the first 2 elements at the top of stack b. Do nothing if there is only one or no elements).
+void	ss(void);  //sa and sb at the same time.
+void	pa(void);  //push a - take the first element at the top of b and put it at the top of a. Do nothing if b is empty.
+void	pb(void);  //push b - take the first element at the top of a and put it at the top of b. Do nothing if a is empty.
+void	ra(void);  //rotate a - shift up all elements of stack a by 1. The first element becomes the last one.
+void	rb(void);  //rotate b - shift up all elements of stack b by 1. The first element becomes the last one.
+void	rr(void);  //ra and rb at the same time.
+void	rra(void);   //reverse rotate a - shift down all elements of stack a by 1. The last element becomes the first one.
+void	rrb(void);  //reverse rotate b - shift down all elements of stack b by 1. The last element becomes the first one.
+void	rrr(void); //rra and rrb at the same time.
