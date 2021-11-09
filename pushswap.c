df9d3e3c31b526c1873e46f9bf725f90155ed5fc
Dejan Zivanov <punisher0504@gmail.com>
2021-11-09 21:09:39 +0100

    Adding Requirements

--- pushswap.c
+++ pushswap.c
@@ -6,7 +6,7 @@
 /*   By: dzivanov <dzivanov@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/09 20:19:12 by dzivanov          #+#    #+#             */
-/*   Updated: 2021/11/09 20:26:57 by dzivanov         ###   ########.fr       */
+/*   Updated: 2021/11/09 21:09:16 by dzivanov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,3 +14,25 @@ int main(int argc, char **argv)
 {
 	//adding test commit
 }
+
+
+
+
+/*
+**	Already Sorted
+**	Errong Handling / No Arguments
+**	Establish the rules
+**
+**	sa : swap a - swap the first 2 elements at the top of stack a. Do nothing if there is only one or no elements).
+**	sb : swap b - swap the first 2 elements at the top of stack b. Do nothing if there is only one or no elements).
+**	ss : sa and sb at the same time.
+**	pa : push a - take the first element at the top of b and put it at the top of a. Do nothing if b is empty.
+**	pb : push b - take the first element at the top of a and put it at the top of b. Do nothing if a is empty.
+**	ra : rotate a - shift up all elements of stack a by 1. The first element becomes the last one.
+**	rb : rotate b - shift up all elements of stack b by 1. The first element becomes the last one.
+**	rr : ra and rb at the same time.
+**	rra : reverse rotate a - shift down all elements of stack a by 1. The last element becomes the first one.
+**	rrb : reverse rotate b - shift down all elements of stack b by 1. The last element becomes the first one.
+**	rrr : rra and rrb at the same time.
+**
+*/
